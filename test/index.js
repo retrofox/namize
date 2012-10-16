00b76af10c318dad408cb03118309ed846af4f30
Damian Suarez <rdsuarez@gmail.com>
2012-10-16 10:20:57 -0300

    test: add tests

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var namize = require('../');
+
+describe('standarize(string, 20)', function(){
+  it('should return a string equal to \'Rene Geronimo Favaloro,\'', function(){
+    namize('RENE GERONIMO FAVALORO').should.be.equal('Rene Geronimo Favaloro');
+    namize('rene geronimo favaloro').should.be.equal('Rene Geronimo Favaloro');
+    namize('reNe geRonImo Favaloro').should.be.equal('Rene Geronimo Favaloro');
+  });
+});
